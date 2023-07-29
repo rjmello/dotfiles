c034a67b315870dbee91a7c91623fd6ebda5b38f
Reid Mello <30907815+rjmello@users.noreply.github.com>
2023-07-29 02:58:43 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DOTFILES="
+	bashrc
+	gitconfig
+	tmux.conf
+"
+
+
+cd $(dirname $0)
+
+
+echo
+echo "Installing dotfiles ..."
+for file in $DOTFILES; do
+	echo "Linking $file"
+	ln -sfT $PWD/$file ~/.$file
+done
+
+
+echo 
+echo "Installing bin files ..."
+pushd bin
+mkdir -p ~/bin
+for file in *; do
+	echo "Linking $file"
+	ln -sfT $PWD/$file ~/bin/$file
+done
+popd
